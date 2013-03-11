4657583cc3decb4f0684e12a4b619b9e8b084f78
Klim Zavadski <klim_911@tut.by>
2013-03-11 11:22:13 +0300

    Added Token Ring logic and changed UI.

--- Lab_6 - Token Ring/custom.js
+++ Lab_6 - Token Ring/custom.js
@@ -1,56 +1,104 @@
-var isMonitor = "IsMonitor";
-var tokenObject = "tokenObject";
-var reset = "Reset";
+var debug = true;
 
-var stack = new Array();
+var monitorObject = "monitorObject";
+var tokenObject = "tokenObject";
+var stationObject = "stationObject";
+
+var isMonitor = false;
+var stations = new Array();
+var marker = {
+	priority: 2,
+	reservedPriority: 0,
+	isMonitor: false,
+	isMarker: true,
+
+	data: "",
+	destination: 0,
+	source: 0,
+	isRead: false
+};
+
+var address;
+var stationId = ParseInt(Math.random() * 1000);
+var timeStart = null;
+var timeLimit = 300;
+var countSendedSymbols;
 
 
 $(document).ready(function () {
-	Initialize();
-});
-
-function Initialize() {
 	window.addEventListener('storage', ReceiveData, false);
-	ClearSendTextArea();
-	
-	if (!localStorage.getItem(isMonitor))
+
+	if (!localStorage.getItem(monitorObject))
 	{
-		localStorage.setItem(isMonitor, true);
-		$('#sendtokenButton').attr('disabled', false);
+		localStorage.setItem(monitorObject, false);
+		isMonitor = true;
+		stations.push({
+			id: stationId,
+			address: address
+		});
+		$('#sendTokenButton').css('visibility', 'visible');
 	}
 
-	/*$('#sendTextArea').keyup(function () {
-		if ($(this).val().length == 0) {
-			$('#sendButton').attr('disabled', true);
-		}
-		else {
-			$('#sendButton').attr('disabled', false);
-		}
-	});*/
+	$('#receiveAddress').change(function () {
+		address = $(this).val();
+		Debug(address);
+		localStorage.setItem(stationObject, JSON.stringify({
+			id: stationId,
+			address: address
+		}));
+	});
 
 	$('#sendtokenButton').click(function () {
-		//
+		stations.sort();
+		SendToken({
+			stations: stations,
+			token: marker,
+			index: 0
+		});
 	});
 
 	$('#resetButton').click(function () {
-		localStorage.setItem(reset, true);
 		localStorage.clear();
-		ResetTextArea();
 	});
+});
+
+
+function SendToken(object)
+{
+	if (object.stations.length > object.index)
+	{		
+		localStorage.setItem(tokenObject, JSON.stringify(object));
+	}
+	else
+	{
+		//object.token.isMonitor = true;
+		object.index = 0;
+		SendToken(object);
+	}
 }
 
-
-function SendData()
+function SendData(object)
 {
-	var data = $('#sendTextArea').val();
-		
-	if (data.length != 0)
+	if (startTime == null)
 	{
-		var token = {
-			//id: id,
-		};
+		startTime = new Date();
+		countSendedSymbols = 0;
 
-		localStorage.setItem(tokenObject, token);
+		object.token.reservedPriority = 0;
+	}
+	if (startTime + timeLimit < new Date())
+	{
+		object.token.priority = $('#stationPriority option:selected').val();
+		object.token.isMonitor = false;
+		object.token.isMarker = false;
+
+		object.token.data = $('#sendTextArea').val()[countSendedSymbols];
+		object.token.destination = $('#sendAddress').val();
+		object.token.source = address;
+		object.token.isRead = false;
+		SendData(marker);
+
+		countSendedSymbols++;
 	}
 }
 
@@ -58,11 +106,70 @@ function ReceiveData(event)
 {
 	if (event.key == tokenObject)
 	{
-		//
+		var object = JSON.parse(event.newValue);
+		// This is for this station.
+		if (object.stations[object.index] == address)
+		{
+			// This is marker.
+			if (object.token.isMarker)
+			{
+				Debug("Get marker");
+				var priority = $('#stationPriority option:selected').val();
+
+				if (priority < object.token.priority)
+				{
+					if (priority > object.token.reservedPriority)
+					{
+						Debug("Change priority");
+						object.token.reservedPriority = priority;
+					}
+					SendToken(object);
+				}
+				else
+				{
+					Debug("Send data");
+					SendData(object);  //!!!!!!!!!!!!!!!!
+				}
+			}
+			// This is frame for us.
+			else if (object.token.destination == address)
+			{
+				Debug("Get token");
+				AddText('', object.token.data + "  ");
+
+				object.token.isRead = true;
+				SendToken(object);
+			}
+			// Token is back to station.
+			else if (object.token.source == address)
+			{
+				Debug("Token is back.");
+
+				object.token.reservedPriority = 0;
+				SendData(object);
+			}
+		}
 	}
-	else if (event.key == reset)
+	// Change or add new station address in the network.
+	else if (event.key == stationObject && isMonitor)
 	{
-		ResetTextArea();
+		var station = JSON.parse(event.newValue);
+		Debug("Get addr=" + station.address);
+		
+		var index = null;
+		$(stations).each(function (i, elem) {
+			if (elem.id == station.id)
+				index = i;
+		});
+
+		if (index != null)  // Change old address.
+		{
+			stations[index] = station;
+		}
+		else
+		{
+			stations.push(station);
+		}
 	}
 }
 
@@ -73,20 +180,6 @@ function AddText(textAreaName, text)
 	$(textAreaName).val(textArea.val() + text);
 }
 
-function ResetTextArea()
-{
-	$('#systemTextArea').val("");
-	$('#receiveTextArea').val("");
-	Sleep(Math.random() * 1000);		// Sleeping for good activation.
-	window.location.reload();
-}
-
-function ClearSendTextArea()
-{
-	$('#sendtokenButton').attr('disabled', true);
-	$('#sendTextArea').val("");
-}
-
 function Sleep(millis)
 {
 	var date = new Date();
@@ -95,4 +188,14 @@ function Sleep(millis)
 	{
 		curDate = new Date();
 	} while(curDate - date < millis);
+}
+
+
+
+function Debug(text)
+{
+	if (debug)
+	{
+		AddText('#systemTextArea', text + '  ');
+	}
 }
